a94be58df7c41fe0b61259669445f6db86d43e6a
Gaston Courtade <courtade.gaston@gmail.com>
2020-04-15 22:23:05 +0200

    Commit

---
+++ update.sh
@@ -0,0 +1,7 @@
+cp /home/gaston/OneDriveNTNU/Kurs_undervisning/BioCAT-NMR_april20/exercise/titration/*ipynb .
+cp -r /home/gaston/OneDriveNTNU/Kurs_undervisning/BioCAT-NMR_april20/exercise/titration/images .
+
+git add --all
+git commit -m "Commit"
+git push -u origin master
+
