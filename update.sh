256abf066bc7260359ab03c29e3005d16335fc20
gcourtade <47104451+gcourtade@users.noreply.github.com>
2020-04-16 13:19:06 +0200

    Delete update.sh

--- update.sh
+++
@@ -1,7 +0,0 @@
-cp /home/gaston/OneDriveNTNU/Kurs_undervisning/BioCAT-NMR_april20/exercise/titration/*ipynb BioCat-nmr-titration.ipynb 
-cp -r /home/gaston/OneDriveNTNU/Kurs_undervisning/BioCAT-NMR_april20/exercise/titration/images .
-
-git add --all
-git commit -m "Commit"
-git push -u origin master
-
